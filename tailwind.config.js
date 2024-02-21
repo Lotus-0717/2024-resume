72f73a22b86e69a89b3448d96afd63ace40c8e23
lotus <lian82717pro@gmail.com>
2024-02-21 20:58:53 +0800

    feat: customize Tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "375px",
+      },
+      colors: {
+        light: "var(--color-theme-light)",
+        dark: "var(--color-theme-dark)",
+        primary: "var(--color-theme-primary)",
+        secondary: "var(--color-theme-secondary)",
+        accent: "var(--color-theme-accent)",
+      },
+    },
   },
   plugins: [],
 };
