15882846810d50c8f2d37efa037225cae33a9076
lotus <lian82717pro@gmail.com>
2024-02-21 22:12:46 +0800

    refactor: Change structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import MainConatiner from "./components/container/MainContainer";
+import MainConatiner from "@components/container/MainContainer";
 function App() {
   return (
-    <>
+    <div className="w-full bg-dark">
       <MainConatiner></MainConatiner>
-    </>
+    </div>
   );
 }
 
