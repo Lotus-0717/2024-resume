804183f1695d0e34aeff7ea452e534ad7913400e
lotus <lian82717pro@gmail.com>
2024-02-21 20:57:36 +0800

    feat: configure path aliases in Vite project settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      // 這裡可以根據需要添加更多的縮寫
+    },
+  },
 })
