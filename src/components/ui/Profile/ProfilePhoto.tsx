073bc19689ce977a1450e271a4ac35eabc47c926
lotus <lian82717pro@gmail.com>
2024-02-22 21:12:01 +0800

    fix: fix pic style

--- src/components/ui/Profile/ProfilePhoto.tsx
+++ src/components/ui/Profile/ProfilePhoto.tsx
@@ -3,7 +3,7 @@ import pic from "@assets/pic.png";
 function ProfileInfo() {
   return (
     <>
-      <div className="h-52 w-52 overflow-hidden rounded-full sm:px-8 ">
+      <div className="h-52 w-52 overflow-hidden rounded-full">
         <img src={pic} alt="" />
       </div>
     </>
