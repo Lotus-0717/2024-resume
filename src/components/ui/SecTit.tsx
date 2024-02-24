68a2e53f73e6cc5686d95e11a606a731e5f73289
lotus <lian82717pro@gmail.com>
2024-02-24 11:27:04 +0800

    feat: Set section title new style

--- src/components/ui/SecTit.tsx
+++ src/components/ui/SecTit.tsx
@@ -3,9 +3,17 @@ interface SecTitProps {
 }
 function SecTit({ children }: SecTitProps) {
   return (
-    <p className="text-4xl font-bold uppercase italic text-secondary ">
-      {children}
-    </p>
+    <div className="relative left-4">
+      <span className="font-outline-2 absolute -left-4 -top-4 text-4xl font-bold uppercase italic text-transparent">
+        {children}
+      </span>
+      <span className="font-outline-2 absolute -left-2 -top-2 text-4xl font-bold uppercase italic text-transparent">
+        {children}
+      </span>
+      <p className="relative z-10 text-4xl font-bold uppercase italic text-secondary">
+        {children}
+      </p>
+    </div>
   );
 }
 
